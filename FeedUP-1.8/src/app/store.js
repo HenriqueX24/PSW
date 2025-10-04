9838fd8bcd7b16177211c9e8e36f759f2129b6a9
Rafael Henrique <131375370+rafahcs@users.noreply.github.com>
2025-10-04 13:25:26 -0300

    add redux em FeedUP-1.8

--- FeedUP-1.8/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import usersReducer from '../features/user/usersSlice'; 
-
-export default configureStore({
-  reducer: {
-    users: usersReducer, 
-  },
-});
