3111c4a9d1f26cbb2461094a491d2a0ed43269ea
Henrique <phpeixotoa@gmail.com>
2025-10-05 13:41:18 -0300

    mudando nome de app Para App

--- FeedUP-1.10/src/main.jsx
+++ FeedUP-1.10/src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import App from './app.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
