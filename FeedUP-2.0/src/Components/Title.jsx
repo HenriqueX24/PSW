aea7d08cc3d6c738819e7fd65f40acdba5300955
Rafael Henrique <rjrafahenrique@gmail.com>
2025-10-07 15:01:06 -0300

    add title component e FeedUP-2.0

---
+++ FeedUP-2.0/src/Components/Title.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../pages/Avaliacao/avaliacaostyle.css";
+import { Container, Typography } from '@mui/material';
+
+export default function Title({titulo}) {
+
+  return (
+    <Typography
+              variant="h1"
+              sx={{
+                lineHeight: 1.2,
+                color: "var(--brand)",
+                fontSize: "2.5rem",
+                fontWeight: 300,
+                margin: 0 /* remove o hack do margin negativo */,
+              }}
+            >
+              {titulo}
+    </Typography>
+
+  )
+}
