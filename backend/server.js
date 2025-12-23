5b08295b996acfb3834f23d4d28abdef8e000d95
Henrique <phpeixotoa@gmail.com>
2025-12-23 03:55:39 -0300

    terminado build pro render

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware primeiro
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: ["hhttps://front-a7ua.onrender.com"],
+  credentials: true
+}));
+
 // Mongo
 const uri = process.env.DATABASE_URL;
 
